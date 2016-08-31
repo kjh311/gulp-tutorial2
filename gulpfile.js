2ba3a296f591c0ef66ba391cebbdf1564eeda938
Kevin Huelsmann <kjh311@gmail.com>
2016-08-31 09:05:43 -0700

    browser sync works with changes on scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,19 @@ gulp.task('sass', function(){
     sass('scss/styles.scss')
         .on('error', sass.logError)
         .pipe(gulp.dest('css'))
+        // injects changes into browser when change of scss:
+        .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('watch', function(){
   gulp.watch('scss/styles.scss', ['sass']);
+});
+
+browserSync.init({
+    // injects changes instead of browser reload
+    injectChanges: true,
+    // base location of index.html:
+    server: "."
 });
 
 gulp.task('browserSync', function() {
